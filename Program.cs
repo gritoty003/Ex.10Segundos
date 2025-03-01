c9ecad8bbbe91fb8cfe7e03c34b28e1a5d29065c
guilherme domingoscls <guilherme.domingos240503@gmail.com>
2025-02-28 21:53:38 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Contando até Dez: 0");
+
+  Thread.Sleep(1000); Console.Write(" 1");
+  Thread.Sleep(1000); Console.Write(" 2");
+  Thread.Sleep(1000); Console.Write(" 3");
+  Thread.Sleep(1000); Console.Write(" 4");
+  Thread.Sleep(1000); Console.Write(" 5");
+  Thread.Sleep(1000); Console.Write(" 6");
+  Thread.Sleep(1000); Console.Write(" 7");
+  Thread.Sleep(1000); Console.Write(" 8");
+  Thread.Sleep(1000); Console.Write(" 9");
+  Thread.Sleep(1000); Console.Write(" 10");
+
+     Thread.Sleep(3000); Console.Clear();
